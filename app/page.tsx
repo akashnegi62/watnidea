6d9e282026ab8da67afef397bc16f9c4a055204a
akashnegi62 <akashsing215@gmail.com>
2026-05-06 15:40:03 +0530

    Loadern Add

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import Loader from "@/components/Loader";
+import Loader from "@/components/Loader";
 import HeroPage from "@/components/HeroSec";
 import AboutSec from "@/components/AboutSec";
 import SectionSec from "@/components/ServiceSec";
@@ -9,7 +9,7 @@ import StrategicSec from "@/components/StrategicSec";
 function page() {
   return (
     <>
-      {/* <Loader /> */}
+      <Loader />
       <HeroPage />
       <AboutSec />
       <SectionSec />
